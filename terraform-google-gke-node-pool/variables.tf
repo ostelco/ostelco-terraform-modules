11fe401a56ba41f30f868bb3f977f869c0966515
sami-alajrami <sami@praqma.com>
2018-09-06 13:58:08 +0200

    updating default oauth scopes for node pools

--- terraform-google-gke-node-pool/variables.tf
+++ terraform-google-gke-node-pool/variables.tf
@@ -58,7 +58,6 @@ variable "oauth_scopes" {
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management",
     ]
 }
 
