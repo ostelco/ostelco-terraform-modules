810462e7adf89d72924ca994d43ec027875eeae6
sami-alajrami <sami@praqma.com>
2018-09-06 13:47:59 +0200

    adding oauth for service management to node pools

--- terraform-google-gke-node-pool/main.tf
+++ terraform-google-gke-node-pool/main.tf
@@ -23,6 +23,7 @@ resource "google_container_node_pool" "np" {
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management",
     ]
 
     disk_size_gb = "${var.pool_node_disk_size_gb}"
